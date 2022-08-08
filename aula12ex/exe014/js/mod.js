9176bc4015a468d319fc32adbaed75c335646082
Fabio Santos <fabio3-9@outlook.com>
2022-08-07 22:56:34 -0300

    exercicio realizado

---
+++ aula12ex/exe014/js/mod.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = 22
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        img.src = "imagem/fotomanha.png"
+        document.body.style.background = '#FBB266'
+    } else if (hora >= 12 && hora < 18){
+        img.src = "imagem/fototarde.png"
+        document.body.style.background = '#924B2C'
+    } else {
+        img.src = "imagem/fotonoite.png"
+        document.body.style.background = '#515154'
+        
+    }   
+}
