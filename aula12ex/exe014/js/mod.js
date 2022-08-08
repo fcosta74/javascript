a9f443efd3eb6befcb7c63791628477cc739a72f
Fabio Santos <fabio3-9@outlook.com>
2022-08-07 23:30:16 -0300

    Update mod.js

--- aula12ex/exe014/js/mod.js
+++ aula12ex/exe014/js/mod.js
@@ -2,7 +2,7 @@ function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    var hora = 22
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12){
         img.src = "imagem/fotomanha.png"
