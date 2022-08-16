f70cdc96723b157dd1dee76d26490d695df2d869
Fabio Santos <fabio3-9@outlook.com>
2022-08-16 20:25:46 -0300

    desafios realizado com sucesso

--- Desafios/desafio13/script.js
+++ Desafios/desafio13/script.js
@@ -2,30 +2,47 @@ function validar(){
     let vr1 = Number(prompt('Qual o preço anterior do produto'))
     let vr2 = Number(prompt('Qual o preço atual do produto'))
     let res = document.getElementById('res')
-
-    dif1 = vr2 - vr1
-    dif2 = vr1 - vr2
-    por1 = dif2 / vr2 * 100
-    por2 = dif1 / vr1 * 100
-
+     
+    dif1 = vr1 - vr2
+    dif2 = vr2 - vr1
+    porc1 = dif2 / vr1 * 100
+    porc2 = dif1 / vr2 * 100
+    
     let valorAtual1 = vr1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
     let valorAtual2 = vr2.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
     let difAtual1 = dif1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
     let difAtual2 = dif2.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+
+    res.innerHTML = '<h1>Analisando os valores informados...</h1>'
+    res.innerHTML += `<p>O produto custava ${valorAtual1} e agora custa ${valorAtual2}.</p>`
+
     
-    if (valorAtual1 < valorAtual2){
-        res.innerHTML = '<h1>Analisando os valores informados...</h1>'
-        res.innerHTML += `<p>O produto custava ${valorAtual1} e agora custa ${valorAtual2}.</p>`  
+    if (vr1 < vr2){          
         res.innerHTML += '<p>Hoje o produto está mais caro.</p>'
-        res.innerHTML += `<p>O preço subiu ${difAtual1} em relação ao preço anterior.</p>`
-        res.innerHTML += `<p>Uma variação de ${por2.toFixed(2)}% pra cima.</p>`
-    } else {
-        res.innerHTML = '<h1>Analisando os valores informados...</h1>'
-        res.innerHTML += `<p>O produto custava ${valorAtual1} e agora custa ${valorAtual2}.</p>`  
+        res.innerHTML += `<p>O preço subiu ${difAtual2} em relação ao preço anterior.</p>`
+        res.innerHTML += `<p>Uma variação de ${porc1.toFixed(2)}% pra cima.</p>`
+    } else if (vr1 > vr2) { 
         res.innerHTML += '<p>Hoje o produto está mais barato.</p>'
-        res.innerHTML += `<p>O preço caiu ${difAtual2} em relação ao preço anterior.</p>`
-        res.innerHTML += `<p>Uma variação de ${por1.toFixed(2)}% pra baixo.</p>`
+        res.innerHTML += `<p>O preço caiu ${difAtual1} em relação ao preço anterior.</p>`
+        res.innerHTML += `<p>Uma variação de ${porc2.toFixed(2)}% pra baixo.</p>`
+    } else {
+        res.innerHTML += `<p>Não houve variação de preços</p>`
     }
-   
-   
+
+    
+/*
+    if (vr1 < vr2){
+        res.innerHTML = '<h1>Analisando os valores informados...</h1>'
+        res.innerHTML += `<p>O produto custava R$ ${vr1.toFixed(2)} e agora custa R$ ${vr2.toFixed(2)}.</p>`  
+        res.innerHTML += '<p>Hoje o produto está mais caro.</p>'
+        res.innerHTML += `<p>O preço subiu R$ ${dif2.toFixed(2)} em relação ao preço anterior.</p>`
+        res.innerHTML += `<p>Uma variação de ${porc1.toFixed(2)}% pra cima.</p>`
+    } else if (vr1 > vr2){
+        res.innerHTML = '<h1>Analisando os valores informados...</h1>'
+        res.innerHTML += `<p>O produto custava R$ ${vr1.toFixed(2)} e agora custa R$ ${vr2.toFixed(2)}.</p>`  
+        res.innerHTML += '<p>Hoje o produto está mais barato.</p>'
+        res.innerHTML += `<p>O preço caiu R$ ${dif1.toFixed(2)} em relação ao preço anterior.</p>`
+        res.innerHTML += `<p>Uma variação de ${porc2.toFixed(2)}% pra baixo.</p>`
+    }
+*/
 }
---
+++ Desafios/desafio14/script.js
@@ -0,0 +1,22 @@
+function checar(){
+    let aluno = prompt('Qual é o nome do aluno?')
+    let n1 = Number(prompt(`Primeira nota de ${aluno}`))
+    let n2 = Number(prompt(`Segunda nota de ${aluno}`))
+    
+    res.innerHTML = document.querySelector('res')
+    media = (n1 + n2) / 2
+    res.innerHTML = `<h2>Analisando a situação de ${aluno}...</h2>`
+    res.innerHTML += `<p>Com as notas ${n1} e ${n2} a <strong>média é ${media}</strong>.</p>`
+    
+    if (media > 6) {
+        res.innerHTML +='Com a média acima de 6,0, o aluno está <strong><mark class="apr">APROVADO</mark></strong>.'
+    
+    } else if (media >= 3.0 && media <= 6.00){
+        res.innerHTML +='Com a média entre 3,0 e 6,0, i aluno está em <strong><mark class="rec">RECUPERAÇÃO</mark></strong>.'
+    
+    } else {
+        res.innerHTML +='Com média abaixo de 3,0, o aluno está <strong><mark class="rep">REPROVADO</mark></strong>.'
+        
+    }
+}
+
